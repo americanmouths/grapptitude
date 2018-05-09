1afab30279f74b0e9139dcd1d1b80172517b7cf4
americanmouths <shiverr@gmail.com>
2018-05-09 10:47:33 -0700

    add socialization gem to allow models to like/follow one another

--- app/models/daily_great.rb
+++ app/models/daily_great.rb
@@ -1,3 +1,4 @@
 class DailyGreat < ApplicationRecord
+  act_as_likeable
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  act_as_followable
+  act_as_follower
+  act_as_liker
   has_many :daily_greats
 end
