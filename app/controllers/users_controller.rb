e19237fed9258b7306aeb40a82fc25e98648bfff
americanmouths <shiverr@gmail.com>
2018-05-09 12:32:00 -0700

    add users route, create user and show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid && @user.save
+      render json: @user, status: 200
+    end
+  end
+
+  def show
+    render json: @user
+  end
+
+
+  private
+  def
+    @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   scope '/api' do
     post 'user_token' => 'user_token#create'
+
+    resources :users, only: [:create, :show]
+
     get '/daily_mindfuls' => 'daily_mindfuls#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
