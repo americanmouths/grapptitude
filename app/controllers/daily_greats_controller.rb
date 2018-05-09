f511f46b1b237d8f19bdb69b1ce0d24f9a47195f
americanmouths <shiverr@gmail.com>
2018-05-09 13:23:20 -0700

    start daily greats controller actions

--- app/controllers/daily_greats_controller.rb
+++ app/controllers/daily_greats_controller.rb
@@ -1,2 +1,9 @@
 class DailyGreatsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @dailygreats = current_user.daily_greats
+    render json: @dailygreats
+  end
+
 end
