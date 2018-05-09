cef321f17783258107c3c9113647637d11a8b0ea
americanmouths <shiverr@gmail.com>
2018-05-09 14:21:57 -0700

    build out controller actions, add routes to daily greats

--- app/controllers/daily_greats_controller.rb
+++ app/controllers/daily_greats_controller.rb
@@ -6,4 +6,35 @@ class DailyGreatsController < ApplicationController
     render json: @dailygreats
   end
 
+  def create
+    @dailygreat = DailyGreat.create(daily_great_params)
+    if @dailygreat.valid && @dailygreat.save
+      render json: @dailygreat, status: 200
+    end
+  end
+
+  def show
+    @dailygreat = DailyGreat.find_by(id: params[:id])
+    render json: @dailygreat, status: 200
+  end
+
+  def destroy
+    @dailygreat = DailyGreat.find_by(id: params[:id])
+    @dailygreat.delete
+  end
+
+  def update
+    @dailygreat = DailyGreat.find_by(id: params[:id])
+    @dailygreat.update(daily_great_params)
+    if @dailygreat.save && @dailygreat.valid
+      render json: @dailygreat, status: 200
+    end
+  end
+
+  private
+    def daily_great_params
+      params.require(:dailygreat).permit(:user_id, :content)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     post 'user_token' => 'user_token#create'
 
     resources :users, only: [:create, :show]
+    resources :daily_greats
 
     get '/daily_mindfuls' => 'daily_mindfuls#index'
   end
