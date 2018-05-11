a7290ad1c24f5fa6482c8f94c74455301a189093
americanmouths <shiverr@gmail.com>
2018-05-10 19:13:52 -0700

    build basic app component to test rails api backend, mindfuls loading

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DailyMindfuls from './components/DailyMindfuls'
+import { fetchMindfuls } from './actions/fetchMindfuls'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 
 class App extends Component {
+
+  componentDidMount(){
+    this.props.fetchMindfuls();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h1>Grapptitude</h1>
         </div>
-        <DailyMindfuls />
+        <DailyMindfuls mindfuls={this.props.mindfuls}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    mindfuls: state.mindfuls.mindfuls
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchMindfuls: fetchMindfuls
+  }, dispatch);
+}
+
+export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App)
