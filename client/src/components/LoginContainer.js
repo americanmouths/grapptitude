6db09a47bd1cd48ab290cdd9d885bfa374653626
americanmouths <shiverr@gmail.com>
2018-05-12 16:45:11 -0700

    build out login container, properly logging in user

---
+++ client/src/components/LoginContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { login } from './login';
+
+export default class LoginContainer extends Component {
+
+  state = {
+    email: "",
+    password: ""
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const loginParams = {
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    login(loginParams).then((user) => {
+    localStorage.setItem("jwtToken", user.jwt)
+    localStorage.setItem("id", user.id)
+  }).then(console.log("logged in"))
+}
+
+handleEmail = (event) => {
+  this.setState({email: event.target.value})
+}
+
+handlePassword = (event) => {
+  this.setState({password: event.target.value})
+}
+
+  render() {
+    return (
+        <div>
+          <form>
+            <input type="text" name="email" placeholder="Email" onChange={this.handleEmail} value={this.state.email}/>
+            <br />
+            <input type="password" name="password" placeholder="Password" onChange={this.handlePassword} value={this.state.password}/>
+              <button onClick={this.handleSubmit}> Login </button>
+            <br />
+          </form>
+        </div>
+    );
+  }
+}
