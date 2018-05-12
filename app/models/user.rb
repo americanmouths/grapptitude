53f99b4050ddd91d7dfb3bacceb90a90627d8d03
americanmouths <shiverr@gmail.com>
2018-05-12 16:43:01 -0700

    update acts_as_followable and acts_as_follower methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  act_as_followable
-  act_as_follower
-  act_as_liker
+  acts_as_followable
+  acts_as_follower
+  acts_as_liker
   has_many :daily_greats
 end
