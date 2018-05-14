37aaf494430f482ce10f0fc9cdc1a9d878f617f8
americanmouths <shiverr@gmail.com>
2018-05-14 12:31:32 -0700

    change to display content of mindful, no longer an array to map

--- client/src/components/mindfuls/DailyMindfuls.js
+++ client/src/components/mindfuls/DailyMindfuls.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 
 const DailyMindfuls = ({mindfuls}) => {
 
-  const dailyMindfuls = mindfuls.map((mindful, index) => {
-    return <li key={index}>{mindful.content}</li>
-  })
-
   return(
     <div>
-    {dailyMindfuls}
+    {mindfuls.content}
     </div>
   )
 }
