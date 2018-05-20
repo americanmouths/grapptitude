63b5ac2c1ea6cadc68bd5ae276c6c2fcf1fa6ab8
americanmouths <shiverr@gmail.com>
2018-05-20 15:21:44 -0700

    create action to add new user great

--- client/src/actions/userGreats.js
+++ client/src/actions/userGreats.js
@@ -17,3 +17,22 @@ export function fetchUserGreats() {
       .then(userGreats => dispatch({ type: 'FETCH_USER_DAILY_GREATS', payload: userGreats }));
   };
 }
+
+export function newUserGreat(userGreatData) {
+  const token = localStorage.getItem("token");
+  const id = localStorage.getItem("id");
+  const request = JSON.stringify(userGreatData)
+  return (dispatch) => {
+    dispatch({type: 'ADDING_NEW_USER_GREAT'});
+    return fetch(`/api/users/${id}/daily_greats`, {
+      method: 'post',
+      body: request,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    }).then(response => response.json())
+      .then(userGreats => dispatch({ type: 'ADD_USER_DAILY_GREAT', payload: userGreats }));
+  };
+}
