0e84ce040f559e809cd540e591697502bec9b2e7
americanmouths <shiverr@gmail.com>
2018-05-20 15:24:41 -0700

    add div to user greats display

--- client/src/components/greats/UserGreats.js
+++ client/src/components/greats/UserGreats.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const UserGreats = ({userGreats}) => {
-
   const allUserGreats = userGreats.map((great, index) => {
-    return <p key={index}>{great.content}</p>
+    return <p key={index} className="daily-great-all">{great.content}</p>
   })
 
   return(
