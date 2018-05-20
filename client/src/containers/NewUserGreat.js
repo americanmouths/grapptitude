ad44dbd2a80c5f37002c9e27fd44991b2f3c7c1b
americanmouths <shiverr@gmail.com>
2018-05-20 15:25:31 -0700

    create new user great form, correctly adding to state and displaying

---
+++ client/src/containers/NewUserGreat.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { newUserGreat } from './../actions/userGreats';
+import { bindActionCreators } from 'redux';
+
+class NewUserGreatContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      content: '',
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      content: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.newUserGreat(this.state);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!!nextProps.token) {
+      this.props.history.push('/home')
+    } else {
+      this.setState({
+        content: '',
+      });
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Add a New User Great </label>
+            <input
+              type="text"
+              name="content"
+              value={this.state.content}
+              onChange={(event) => this.handleChange(event)}
+              placeholder="content"
+            />
+            <input type="submit" />
+          </form>
+        </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userGreats: state.userGreats
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    newUserGreat: newUserGreat
+  }, dispatch);
+}
+
+export const NewUserGreat = connect(mapStateToProps, mapDispatchToProps)(NewUserGreatContainer)
