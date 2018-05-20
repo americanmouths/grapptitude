263c7bec2a98c56f39301195f56b9caa6732bd8a
americanmouths <shiverr@gmail.com>
2018-05-20 15:25:55 -0700

    handle adding a new user great to state

--- client/src/reducers/userGreatsReducer.js
+++ client/src/reducers/userGreatsReducer.js
@@ -1,7 +1,12 @@
-export default function userGreatsReducer(state = {loading: false, userGreats: []}, action) {
+export default function userGreatsReducer(state = {
+  loading: false,
+  userGreats: []
+}, action) {
   switch (action.type) {
     case 'FETCH_USER_DAILY_GREATS':
       return {loading: false, userGreats: action.payload}
+    case 'ADD_USER_DAILY_GREAT':
+      return {loading: false, ...state, userGreats: state.userGreats.concat(action.payload)}
     default:
       return state;
   }
