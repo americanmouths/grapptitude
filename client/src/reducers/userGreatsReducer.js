0e87869fdea8e7233307a108e5925dafb9e328cb
americanmouths <shiverr@gmail.com>
2018-05-21 19:00:33 -0700

    add delete action type, properly deleting user great

--- client/src/reducers/userGreatsReducer.js
+++ client/src/reducers/userGreatsReducer.js
@@ -7,6 +7,8 @@ export default function userGreatsReducer(state = {
       return {loading: false, userGreats: action.payload}
     case 'ADD_USER_DAILY_GREAT':
       return {loading: false, ...state, userGreats: state.userGreats.concat(action.payload)}
+    case 'DELETE_USER_DAILY_GREAT':
+      return{loading: false, ...state, userGreats: action.JSON.userGreats}
     default:
       return state;
   }
