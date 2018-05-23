049864e180131a068c1d54bb5fa972f38ab4ec7e
americanmouths <shiverr@gmail.com>
2018-05-22 19:28:17 -0700

    change how all greats are displayed

--- client/src/components/greats/AllGreats.js
+++ client/src/components/greats/AllGreats.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AllGreats = ({greats}) => {
 
 const allGreats = greats.map((great, index) => {
-  return <div key={index} className="daily-great-all">{great.content} - {great.user.username}</div>
+  return <div key={index} className="daily-great-all">{great.user.username} says: <p>{great.content}</p></div>
 })
   return(
     <div>
