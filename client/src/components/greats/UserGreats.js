989783f0e91741135cec954985f27ee52cb5c65c
americanmouths <shiverr@gmail.com>
2018-05-24 20:28:52 -0700

    display date user great created

--- client/src/components/greats/UserGreats.js
+++ client/src/components/greats/UserGreats.js
@@ -3,7 +3,7 @@ import React from 'react';
 const UserGreats = (props) => {
   const allUserGreats = props.userGreats.map((great, index) => {
     const id = great.id
-    return <div><p key={index} className="daily-great-all">{great.content}<br /><button onClick={() => props.deleteUserGreat(id)}>Delete</button></p></div>
+    return <div><p key={index} className="daily-great-all">{great.content}<br />created at {great.date_created}<br /><br /><button onClick={() => props.deleteUserGreat(id)}>Delete</button></p></div>
   })
 
   return(
