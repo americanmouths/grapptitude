fa52a5dfc937a37bc3dda5f4565136d897acc414
americanmouths <shiverr@gmail.com>
2018-05-24 21:13:41 -0700

    add like and follow buttons to user display of all greats

--- client/src/components/greats/AllGreats.js
+++ client/src/components/greats/AllGreats.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import {Button, Glyphicon} from 'react-bootstrap/lib'
 
 const AllGreats = ({greats}) => {
 
-const allGreats = greats.map((great, index) => {
-  return <div key={index} className="daily-great-all">On {great.date_created} {great.user.username} says: <p>{great.content}</p></div>
-})
-  return (
-    <div className="row">
-      <div className="col-md-4"></div>
-      <div className="col-md-4">{allGreats}</div>
-      <div className="col-md-4"></div>
-    </div>
-  )
+  const loggedIn = !!localStorage.getItem("token")
+
+  const mainGreats = greats.map((great, index) => {
+    return <div key={index} className="daily-great-all"><p className="Daily-Great-Intro">On {great.date_created} {great.user.username} says: </p><p>{great.content}</p></div>
+  })
+
+  const userGreats = greats.map((great,index) => {
+    return <div key={index} className="daily-great-all"><p className="Daily-Great-Intro">On {great.date_created} {great.user.username} says: </p><p>{great.content}</p><div className="btn-toolbar"><Button bsSize="small"><Glyphicon glyph="thumbs-up" /> Like</Button><Button bsSize="small"><Glyphicon glyph="user" /> Follow</Button></div></div>
+  })
+
+    return (
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">{loggedIn ? userGreats : mainGreats}</div>
+        <div className="col-md-4"></div>
+      </div>
+    )
 }
 
 export default AllGreats;
