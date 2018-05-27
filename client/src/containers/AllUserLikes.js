8f96099b05dee1ea4532e3931dab1a97a0310d46
americanmouths <shiverr@gmail.com>
2018-05-27 16:39:18 -0700

    likes container properly maps state likes to props and passes to user likes container

---
+++ client/src/containers/AllUserLikes.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import AllUserLikes from './../components/greats/AllUserLikes'
+import { fetchUserLikes } from './../actions/greats'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+
+class AllUserLikesContainer extends Component {
+
+  componentDidMount(){
+    this.props.fetchUserLikes();
+  }
+
+  render() {
+    return (
+      <div>
+        <AllUserLikes likes={this.props.likes}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    likes: state.likes.likes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchUserLikes: fetchUserLikes,
+  }, dispatch);
+}
+
+export const Likes = connect(mapStateToProps, mapDispatchToProps)(AllUserLikesContainer)
