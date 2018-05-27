1f6ee67aa09b8270ae0d7dc5006ff21eaea177de
americanmouths <shiverr@gmail.com>
2018-05-27 16:35:02 -0700

    add function to fetch user likes and to like a user great

--- client/src/actions/greats.js
+++ client/src/actions/greats.js
@@ -8,3 +8,36 @@ export function fetchGreats() {
       .then(greats => dispatch({ type: 'FETCH_GREATS', payload: greats }));
   };
 }
+
+export function likeUserGreatFromAPI(greatId, token){
+  const id = localStorage.getItem("id")
+  return (dispatch) => {
+    dispatch({type: 'LIKING_USER_GREAT'});
+    return fetch(`/api/users/${id}/daily_greats/${greatId}/like`, {
+      method: 'post',
+      body: JSON.stringify({daily_great_id: greatId}),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    }).then(response => response.json())
+      .then(JSON => dispatch({
+        type: 'LIKE_USER_DAILY_GREAT',
+        JSON: JSON
+      }));
+  };
+}
+
+export function fetchUserLikes(){
+  const id = localStorage.getItem("id")
+  return (dispatch) => {
+    dispatch({type: 'LOADING_LIKES'});
+    return fetch(`/api/users/${id}/likes`)
+    .then(response => response.json())
+      .then(likes => dispatch({
+        type: 'FETCH_USER_LIKES',
+        payload: likes
+      }));
+  };
+}
