478b62a47091601e7babaabed1ca3590dd603fd4
americanmouths <shiverr@gmail.com>
2018-05-28 16:57:50 -0700

    rename files to make more sense, no new code

---
+++ client/src/containers/AllUserGreats.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import UserGreats from './../components/greats/UserGreats';
+import NewGreatHeader from './../components/headers/NewGreatHeader';
+import { NewUserGreat } from './NewUserGreat';
+import { fetchUserGreats, deleteUserGreatFromAPI } from './../actions/userGreats';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+
+class UserDailyGreatsContainer extends Component {
+
+  componentDidMount(){
+    this.props.fetchUserGreats();
+  }
+
+  deleteUserGreat = (userGreatId) => {
+    const token = localStorage.getItem('token')
+    this.props.deleteUserGreatFromAPI(userGreatId, token)
+  }
+
+  render() {
+    return (
+      <div>
+        <NewGreatHeader />
+        <NewUserGreat />
+        <UserGreats userGreats={this.props.userGreats} deleteUserGreat={this.deleteUserGreat}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userGreats: state.userGreats.userGreats,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchUserGreats: fetchUserGreats,
+    deleteUserGreatFromAPI: deleteUserGreatFromAPI
+  }, dispatch);
+}
+
+export const AllUserGreats = connect(mapStateToProps, mapDispatchToProps)(UserDailyGreatsContainer)
---
+++ client/src/containers/UsersHome.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import FollowedUsers from './../components/greats/FollowedUsers'
+import { fetchFollowedUsers } from './../actions/greats'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+
+class UsersHomeContainer extends Component {
+
+  componentDidMount(){
+    this.props.fetchFollowedUsers();
+  }
+
+  render() {
+    return (
+      <div>
+        <FollowedUsers users={this.props.users}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.followedUsers.followedUsers
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchFollowedUsers: fetchFollowedUsers,
+  }, dispatch);
+}
+
+export const UsersHome = connect(mapStateToProps, mapDispatchToProps)(UsersHomeContainer)
