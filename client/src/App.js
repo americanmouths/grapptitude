f2f9451089a56cc8a4e5de6dbbf8268774104601
americanmouths <shiverr@gmail.com>
2018-05-28 16:54:38 -0700

    fix routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './containers/NavBar';
 import LogInPage from './containers/LogInPage';
 import SignUpPage from './containers/SignUpPage';
-import { Home } from './containers/Home';
-import { Greats } from './containers/Greats';
+import { UsersHome } from './containers/UsersHome';
+import { AllAppGreats } from './containers/AllAppGreats';
 import { Likes } from './containers/AllUserLikes';
-import { Users } from './containers/FollowedUsers';
+import { AllUserGreats } from './containers/AllUserGreats';
 
 class App extends Component {
 
@@ -18,15 +17,14 @@ class App extends Component {
       <div>
           <NavBar />
         <Switch>
-          <Route exact path="/" component={Greats} />
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={AllAppGreats} />
+          <Route exact path="/home" component={UsersHome} />
           <Route exact path="/login" component={LogInPage} />
           <Route exact path="/signup" component={SignUpPage} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/liked_posts" component={Likes} />
-          <Route exact path="/followed_users" component={Users} />
+          <Route exact path="/daily_great/liked" component={Likes} />
+          <Route exact path="/daily_great/new" component={AllUserGreats} />
+          <Route exact path="/following" component={UsersHome} />
         </Switch>
-
       </div>
     </Router>
     );
