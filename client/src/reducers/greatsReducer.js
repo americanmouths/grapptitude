0b97743dae9c9daf7e419ef510a05f09272040da
americanmouths <shiverr@gmail.com>
2018-05-27 18:14:06 -0700

    add fetch followed users case statement

--- client/src/reducers/greatsReducer.js
+++ client/src/reducers/greatsReducer.js
@@ -11,6 +11,11 @@ export default function greatsReducer(state = {loading: false, greats: [], likes
      case 'FETCH_USER_LIKES':
      return {loading: false, likes: action.payload}
      case 'FOLLOW_USER':
+     return {loading: false,
+       ...state,
+       followedUsers: [action.payload]
+     }
+     case 'FETCH_FOLLOWED_USERS':
      return {loading: false, followedUsers: action.payload}
     default:
       return state;
