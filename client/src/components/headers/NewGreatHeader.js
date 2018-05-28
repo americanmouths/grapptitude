f3a5b68580624a0669df78b40dd55a3176426f2b
americanmouths <shiverr@gmail.com>
2018-05-28 16:57:25 -0700

    create header for new form

---
+++ client/src/components/headers/NewGreatHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NewGreatHeader = () => {
+
+  return(
+      <div className="App">
+        <div className="App-header">
+          <div className="App-title">
+            <h1>Tell me what you are greatful for...</h1>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default NewGreatHeader;
