39df4e428f59782b58f53eea6b16ee9298c468c9
americanmouths <shiverr@gmail.com>
2018-05-28 16:57:12 -0700

    display diff header if logged in

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Header = () => {
 
+const loggedIn = !!localStorage.getItem("token")
+
   return(
       <div className="App">
         <div className="App-header">
           <div className="App-title">
-            <h1>What are you thankful for?</h1>
+            {loggedIn ? <h1>The community is greatful for...</h1> : <h1>What are you thankful for?</h1>}
           </div>
         </div>
       </div>
