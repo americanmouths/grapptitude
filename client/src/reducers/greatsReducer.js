7d2fcb0c5fd6b0a64ef8e0ae7c322205da7a0eaa
americanmouths <shiverr@gmail.com>
2018-05-28 16:54:24 -0700

    add errors to state, pass to props for greats

--- client/src/reducers/greatsReducer.js
+++ client/src/reducers/greatsReducer.js
@@ -1,19 +1,21 @@
-export default function greatsReducer(state = {loading: false, greats: [], likes: [], followedUsers: []}, action) {
+export default function greatsReducer(state = {loading: false, greats: [], likes: [], followedUsers: [], errors: []}, action) {
   switch (action.type) {
     case 'FETCH_GREATS':
-      return {loading: false, greats: action.payload}
+      return {loading: false, greats: action.payload, errors: []}
     case 'LIKE_USER_DAILY_GREAT':
       return {
         loading: false,
         ...state,
-        likes: [action.payload]
+        likes: [action.payload],
+        errors: [action.payload.errors]
       }
      case 'FETCH_USER_LIKES':
-     return {loading: false, likes: action.payload}
+     return {loading: false, likes: action.payload, errors: []}
      case 'FOLLOW_USER':
      return {loading: false,
        ...state,
-       followedUsers: [action.payload]
+       followedUsers: [action.payload],
+       errors: [action.payload.errors]
      }
      case 'FETCH_FOLLOWED_USERS':
      return {loading: false, followedUsers: action.payload}
