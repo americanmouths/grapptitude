cbc769c3e44ce9d84c651ba171a2661cf053792e
americanmouths <shiverr@gmail.com>
2018-05-28 16:57:01 -0700

    rename to all user likes

--- client/src/containers/AllUserLikes.js
+++ client/src/containers/AllUserLikes.js
@@ -14,7 +14,7 @@ class AllUserLikesContainer extends Component {
   render() {
     return (
       <div>
-        <AllUserLikes likes={this.props.likes}/>
+        <AllUserLikes likes={this.props.likes} />
       </div>
     );
   }
