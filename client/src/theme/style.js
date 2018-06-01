9a09a0c0cce9948d6532ee35c63d0a9180fa7983
americanmouths <shiverr@gmail.com>
2018-05-31 19:38:58 -0700

    create exported style components

--- client/src/theme/style.js
+++ client/src/theme/style.js
@@ -11,43 +11,95 @@ export const HeaderStyle = styled.main`
 `;
 
 export const GreatContainer = styled.main`
-  background-color: transparent !important;
-  border: 2.5px solid #FDF5F2;
+  border: 2.5px double #bcdbb6;
   display: block;
   padding-top: 10px;
   padding-bottom: 10px;
   margin: auto;
   margin-top: 20px;
   width: 310px;
-  height: 120px;
   text-align: center;
-  background: #274046;
+  background: #19282b;
+  opacity: 0.75;
 `;
 
 export const Author = styled.main`
-  color: #936a66;
+  color: #b27a74;
   font-family: Montserrat, sans-serif;
   display: inline-block;
   font-weight: 900;
-  padding-right: 23px;
-  font-size: 1.3em;
-  border-bottom: 1px solid #FDF5F2;
+  padding-right: 33px;
+  font-size: 1.1em;
+  border-bottom: 1px solid #bcdbb6;;
+  position: relative;
 `;
 
 export const DateCreated = styled.main`
   color: #BDA765;
   font-family: Montserrat, sans-serif;
   display: inline-block;
-  padding-left: 23px;
+  padding-left: 33px;
   font-weight: 900;
-  font-size: 1.3em;
-  border-bottom: 1px solid #FDF5F2;
+  font-size: 1.1em;
+  border-bottom: 1px solid #bcdbb6;
+  opacity: 1;
 `;
 
 export const DailyGreatContent = styled.main`
   font-weight: 900;
   font-size: 1.3em;
-  color: #8C9C89;
-  padding-top: 10px;
+  color: #FDF5F2;
+  padding-top: 17px;
   height: 50%;
+  position: relative;
+  padding-bottom: 5%;
+`;
+
+export const FormContainer = styled.main`
+  display: block;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin: auto;
+  margin-top: 20px;
+  width: 350px;
+  height: 225px;
+  text-align: center;
+  background: #19282b;
+  opacity: 0.80;
+`;
+
+export const Errors = styled.main`
+  display: block;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border: 2px dotted #bcdbb6;
+  margin: auto;
+  width: 310px;
+  height: 50px;
+  text-align: center;
+  background: black;
+  opacity: 0.80;
+  text-size: 1.75em;
+  color: #b27a74;
+`;
+
+export const AuthErrors = styled.main`
+  color: #b27a74;
+  text-size: 1.75em;
+  text-align: center;
+`;
+
+export const NoFollowedUsers = styled.main`
+  display: block;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin: auto;
+  margin-top: 20px;
+  width: 350px;
+  height: 100px;
+  text-align: center;
+  background: #19282b;
+  opacity: 0.80;
+  color: #FDF5F2;
+  font-size: 1.0em;
 `;
